0cc81ae837160f7511242da0123b79ed32e1cc21
timerix <timerix11@gmail.com>
2022-10-30 14:42:59 +0600

    added error handling to locgen

--- ParadoxRusLocalisationGen.cs
+++ ParadoxRusLocalisationGen.cs
@@ -10,28 +10,36 @@ static class ParadoxRusLocalisationGen
     
     static void Main(string[] args)
     {
-        if (args.Length != 2 || args[0] == "/?" || args[0] == "help" || args[0] == "--help")
+        try
         {
-            Console.WriteLine("[dir with eng localisation] [dir with rus localisation]");
-            return;
-        }
-
-        string engDir = args[0];
-        string rusir = args[1];
-        foreach (string enfFileName in Directory.GetAllFiles(engDir))
-        {
-            string rusFileName = enfFileName
-                .Replace(engDir, rusir)
-                .Replace("l_english", "l_russian");
-            if(!File.Exists(rusFileName))
+            if (args.Length != 2 || args[0] == "/?" || args[0] == "help" || args[0] == "--help")
             {
-                string text = File.ReadAllText(enfFileName)
-                    .Replace("l_english:", "l_russian: ");
-                byte[] bytes = StringConverter.UTF8BOM.GetBytes(text);
-                File.WriteAllBytes(rusFileName, bytes);
-                ColoredConsole.Write("g",$"file {rusFileName} created\n");
+                Console.WriteLine("[dir with eng localisation] [dir with rus localisation]");
+                return;
             }
-            else ColoredConsole.Write("y", $"file {rusFileName} already exists\n");
+
+            string engDir = args[0];
+            string rusir = args[1];
+            foreach (string enfFileName in Directory.GetAllFiles(engDir))
+            {
+                string rusFileName = enfFileName
+                    .Replace(engDir, rusir)
+                    .Replace("l_english", "l_russian");
+                if (!File.Exists(rusFileName))
+                {
+                    string text = File.ReadAllText(enfFileName)
+                        .Replace("l_english:", "l_russian: ");
+                    byte[] bytes = StringConverter.UTF8BOM.GetBytes(text);
+                    File.WriteAllBytes(rusFileName, bytes);
+                    logger.Log("g", $"file {rusFileName} created");
+                }
+                else logger.Log("y", $"file {rusFileName} already exists");
+            }
         }
+        catch (Exception ex)
+        {
+            logger.Log("r", $"{ex.Message}\n{ex.StackTrace}");
+        }
+        Console.ResetColor();
     }
 }
